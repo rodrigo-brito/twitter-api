53855fa1ab987a2bed1b75a1d5c9ae81cf72d3d9
Rodrigo Brito <rig.10@hotmail.com>
2016-04-04 21:55:53 -0300

    Inclusão de callbacks e concatenação de timeline

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Twitter 	= require('twitter');
+var bigInt 		= require("big-integer");
+//Arquivo com chaves de acesso
 var config 		= require('./key_access');
 
 var client = new Twitter({
@@ -8,21 +10,42 @@ var client = new Twitter({
 	access_token_secret: config.keys.access_token_secret
 });
 
-var params = {
-	screen_name: 'g1politica',
-	count: 200,
-	exclude_replies: true,
-	include_rts: false,
-	trim_user: true,
-	contributor_details: false
+
+
+var getTimelineTwitter = function(screen_name, max_id, count, callback){
+	var params = {
+		screen_name: screen_name,
+		count: count,
+		exclude_replies: true,
+		include_rts: true,
+		trim_user: true,
+		contributor_details: false
+	};
+	if(max_id != null){
+		params.max_id = max_id;
+	}
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+		if(!error){
+			tweets.forEach(function(tt){
+				console.log(tt.id_str+' : '+tt.text);
+			});
+			if(callback){
+				callback(null, tweets[ tweets.length - 1 ].id_str);
+			}
+		}else{
+			console.log("ERRO: ");
+			console.log(error);
+			if(callback){
+				callback(true);
+			}
+		}
+	});
 };
 
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-	if(!error){
-		tweets.forEach(function(tt){
-			console.log(tt.text);
-		});
-	}else{
-		console.log("ERRO: "+error);
+getTimelineTwitter('g1', null, 5, function(err, last_id){
+	if(!err){
+		var id = bigInt(last_id);
+		var next_id = id.minus(1); //Necessário reduzir em uma unidade o id para não incluir o último retornado
+		getTimelineTwitter('g1', next_id.toString(), 5);
 	}
 });
