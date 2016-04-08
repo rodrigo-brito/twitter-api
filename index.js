fde71743d5b9ea92d196f6efdf90ba84841aa9f3
Rodrigo Brito <rig.10@hotmail.com>
2016-04-07 22:20:54 -0300

    Inserção de twitts capturados no MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var Twitter 	= require('twitter');
+var MongoClient = require('mongodb').MongoClient;
 var bigInt 		= require("big-integer");
-//Arquivo com chaves de acesso
 var config 		= require('./key_access');
+
+//Variáveis
+var user_name 	= 'g1politica';
+var count 		= 200;
+var mongo_url 	= 'mongodb://localhost:27017/Twitter';
 
 var client = new Twitter({
 	consumer_key: config.keys.consumer_key,
@@ -9,6 +14,30 @@ var client = new Twitter({
 	access_token_key: config.keys.access_token_key,
 	access_token_secret: config.keys.access_token_secret
 });
+
+var saveTwitts =  function(twitts, callback){
+	MongoClient.connect(mongo_url, function(err, db) {
+		if(!err){
+			console.log("Conectado ao banco com sucesso");
+			console.log("------------------------------");
+			var collection = db.collection(user_name);
+			collection.insertMany(twitts, function(err, result) {
+				if(!err){
+					db.close();
+					console.log(result.result.n+" Twitts inseridos.");
+					callback(result);
+				}else{
+					console.err(err);
+				}
+			});
+		}else{
+			console.err("Erro ao conectar ao banco de dados.");
+			console.log("-----------------------------------");
+			console.err(err);
+			db.close();
+		}
+	});
+};
 
 var trimEndline = function(text){
 	return text.replace(/\r?\n|\r/g, " ");
@@ -28,17 +57,18 @@ var getTimelineTwitter = function(screen_name, max_id, count, callback){
 	}
 	client.get('statuses/user_timeline', params, function(error, tweets, response){
 		if(!error){
-			tweets.forEach(function(tt){
+			/*tweets.forEach(function(tt){
 				console.log(tt.id_str+' : '+trimEndline(tt.text) );
-			});
-			if(callback){
-				if(tweets.length > 0){
+			});*/
+
+			if(tweets.length > 0){
+				saveTwitts(tweets, function(){
 					var id = bigInt(tweets[ tweets.length - 1 ].id_str);
 					var next_id = id.minus(1); //Necessário reduzir em uma unidade o id para não incluir o último retornado
 					callback(null, next_id.toString());
-				}else{
-					callback(null, null); //Null simboliza que não há mais twitts
-				}
+				});
+			}else{
+				callback(null, null); //Null simboliza que não há mais twitts
 			}
 		}else{
 			console.log("ERRO: ");
@@ -50,8 +80,7 @@ var getTimelineTwitter = function(screen_name, max_id, count, callback){
 	});
 };
 
-var user_name = "RodrigoFBrito";
-var count = 200;
+
 getTimelineTwitter(user_name, null, count, function callback(err, last_id){
 	if(!err){
 		if(last_id != null){
